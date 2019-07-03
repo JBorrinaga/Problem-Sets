8dd4895729bb641af124a31c604876e7ab9d45e6
JBorrinaga <52478506+JBorrinaga@users.noreply.github.com>
2019-07-02 23:18:18 -0600

    Create Cash.c
    Problem Set 1 - Cash.c from Harvard's CS50 online course 
    (https://www.edx.org/course/cs50s-introduction-to-computer-science)
    
    tl;dr
    Implement a program that calculates the minimum number of coins required to give a user change.
    ______________________________________________________________
    $ ./cash
    Change owed: 0.41
    4
    _____________________________________________________________

---
+++ CS50/PSet1/Cash/Cash.c
@@ -0,0 +1,40 @@
+//****************************************************************
+// 
+// John Joshua Borrinaga
+// CS50 - PSet1: Cash
+// https://docs.cs50.net/2018/x/psets/1/cash/cash.html
+//
+// Calculates minimum number of coins 
+// returned from a given change
+// 
+// Modified (02/07/2019):
+// - Outputs total number of pennies, nickels, dimes and quarters
+//
+//****************************************************************
+
+#include <stdio.h>
+
+int main(void) {
+
+	float change;
+
+	do {
+		printf("Enter amount of change due: $");
+		scanf_s("%f", &change);
+	} while (change <= 0);
+
+	int roundedChange = change * 100;
+	int quarters = roundedChange / 25;
+	int dimes = (roundedChange % 25) / 10;
+	int nickels = (roundedChange % 25 % 10) / 5;
+	int pennies = (roundedChange % 25 % 10 % 5);
+
+	printf("Quarters: %i\n", quarters);
+	printf("Dimes: %i\n", dimes);
+	printf("Nickels: %i\n", nickels);
+	printf("Pennies: %i\n", pennies);
+	printf("______________________\n");
+	printf("Total Coins: %i", quarters + dimes + nickels + pennies);
+	_getch();
+
+}
